042b49392e05f5c1764fc8938c5807605ce7b9da
Ryan Gillespie <24619595+nopg@users.noreply.github.com>
2024-06-08 19:26:27 -0600

    Update launcher.sh

--- demo/launcher.sh
+++ demo/launcher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-python3 -m pip install git+https://github.com/glspi/gcli-cfgrepo.git
-git clone https://github.com/glspi/temp-cfgrepo.git
+python3 -m pip install git+https://github.com/nopg/gcli-cfgrepo.git
+git clone https://github.com/nopg/temp-cfgrepo.git
 cd temp-cfgrepo
 cfgrepo load
